878a333696b450299d99789a21762cd7149b86a4
Shokhrukh <uphill7165@gmail.com>
2024-08-21 21:12:15 +0900

    added dynamic routes for each item (meal)

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,5 +1,39 @@
-export default function MealDetails() {
+import { getMeal } from "@/components/lib/meals";
+import classes from "./page.module.css";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+export default function MealDetails({params}) {
+ 
+ const meal =  getMeal(params.slug)
+
+ if(!meal) {
+  notFound()
+ }
+
+ meal.instructions = meal.instructions.replace(/\n/g, "<br/>")
+
   return (
-    <h1>Meals Details</h1>
-  )
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill src={meal.image}/>
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summmary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}></p>
+      </main>
+    </>
+  );
 }
